7e65cfef14f2e025ee4dde1a8c8e405d6980412b
harsh saxena <harshsaxenajii@gmail.com>
2023-03-12 11:51:04 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./Components/Helper/ScrollToTop";
 import Spinner from "./Components/Helper/Spinner";
-
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 const Home = React.lazy(() => import("./Components/Home"));
 const Login = React.lazy(() => import("./Components/Auth/Login"));
 const Register = React.lazy(() => import("./Components/Auth/Register"));
-
 export default function App() {
   return (
     <Router>
